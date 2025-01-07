32c252f669d86a3896f87cb024bdacc942470028
keagan erasmus <keaganerasmus12@gmail.com>
2025-01-07 22:45:30 +1300

    FEAT: Turret mostly implemented

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-var health
-var speed
+var health: int
+var speed: int
 
 @export var en_type: String = "none"
 
 func _ready():
+	add_to_group("enemiesGroup")
 	match en_type:
 		"walker":
 			create_walker()
@@ -19,15 +20,15 @@ func _process(delta):
 
 func create_walker():
 	health = 10
-	speed = 2
+	speed = 20
 	$Sprite2D.modulate = "#D72638"
 	
 func create_runner():
 	health = 5
-	speed = 5
+	speed = 50
 	$Sprite2D.modulate = "#F46036"
 
 func create_tank():
 	health = 20
-	speed = 1
+	speed = 10
 	$Sprite2D.modulate = "#FFD23F"
